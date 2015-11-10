579d5b14266cf6cfa2f014e82d14127f9ade79ac
ababup1192 <ababup1192@gmail.com>
2015-11-10 10:19:46 +0900

    Divide CommentBox Component from Main

--- client/src/main/scala/org/ababup1192/ScalaJSMain.scala
+++ client/src/main/scala/org/ababup1192/ScalaJSMain.scala
@@ -1,15 +1,12 @@
 package org.ababup1192
 
 import japgolly.scalajs.react._
-import japgolly.scalajs.react.vdom.all._
 import org.scalajs.dom.document
 
 import scala.scalajs.js
 
-
 object ScalaJSMain extends js.JSApp {
 
-  case class Comment(author: String, text: String)
 
   override def main(): Unit = {
 
@@ -19,48 +16,8 @@ object ScalaJSMain extends js.JSApp {
       Comment("uni", "Foo!")
     )
 
-    val comment = ReactComponentB[Comment]("Comment")
-      .render($ =>
-        div(className := "comment",
-          h2(className := "commentAuthor",
-            $.props.author
-          ),
-          p(
-            $.props.text
-          )
-        )
-      ).build
-
-    val commentList = ReactComponentB[List[Comment]]("CommentList")
-      .render { $ =>
-        val commentNodes = $.props.map { c =>
-          comment(c)
-        }
-
-        div(className := "commentList",
-          commentNodes
-        )
-      }.build
-
-    val commentForm = ReactComponentB[Unit]("CommentForm")
-      .render(_ =>
-        div(className := "commentForm",
-          "Hello, world! I am a CommentForm."
-        )
-      ).buildU
-
-    val commentBox = ReactComponentB[Unit]("CommentBox")
-      .render($ =>
-        div(className := "commentBox",
-          h1("Components"),
-          commentList(comments),
-          commentForm()
-        )
-      ).buildU
-
-
     ReactDOM.render(
-      commentBox(), document.getElementById("example")
+      CommentBox(comments), document.getElementById("example")
     )
   }
 }
