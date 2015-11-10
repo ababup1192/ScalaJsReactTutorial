45e082b7ca33c0fe0e41057b27e8b313eefec3de
ababup1192 <ababup1192@gmail.com>
2015-11-10 09:37:32 +0900

    Using props

--- client/src/main/scala/org/ababup1192/ScalaJSMain.scala
+++ client/src/main/scala/org/ababup1192/ScalaJSMain.scala
@@ -9,27 +9,44 @@ import scala.scalajs.js
 
 object ScalaJSMain extends js.JSApp {
 
+  case class Comment(author: String, text: String)
+
   override def main(): Unit = {
+
+    val comment = ReactComponentB[Comment]("Comment")
+      .render_P(comment =>
+        div(className := "comment",
+          h2(className := "commentAuthor",
+            comment.author
+          ),
+          p(
+            comment.text
+          )
+        )
+      ).build
+
     val commentList = ReactComponentB[Unit]("CommentList")
       .render(_ =>
         div(className := "commentList",
-          "Hello, world! I am a CommentList."
+          comment(Comment("uni", "hello!")),
+          comment(Comment("kani", "Yeah!")),
+          comment(Comment("uni", "Foo!"))
         )
-      ).build
+      ).buildU
 
     val commentForm = ReactComponentB[Unit]("CommentForm")
       .render(_ =>
         div(className := "commentForm",
           "Hello, world! I am a CommentForm."
         )
-      ).build
+      ).buildU
 
     val commentBox = ReactComponentB[Unit]("CommentBox")
       .render($ =>
         div(className := "commentBox",
           h1("Components"),
-          commentList($),
-          commentForm($)
+          commentList(),
+          commentForm()
         )
       ).buildU
 
